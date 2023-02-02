352212b737a3cca0127f1b3855311e440f11532d
Chaw Thiri San <chawthiri177@gmail.com>
2023-02-02 19:48:24 +0900

    remove duplicates

--- three largest number/solution.py
+++ three largest number/solution.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def findThreeLargestNumbers(array):
     max_list = []
     for i in range(3):
@@ -9,16 +9,5 @@ def findThreeLargestNumbers(array):
     return final_list
 array = input('Type in the array : ').split(',')
 array = list(map(int,array))
-=======
-def findThreeLargestNumbers(array):
-    max_list = []
-    for i in range(3):
-        max_num = max(array)
-        max_list.append(max_num)
-        array.remove(max_num)
-    final_list = sorted(max_list)
-    return final_list
-array = input('Type in the array : ').split(',')
-array = list(map(int,array))
->>>>>>> d78acb4c6da258794be158e5786ea3c7606f7b1e
+
 print(findThreeLargestNumbers(array))

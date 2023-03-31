c207c46ff324c43e127087a6a4a6f798b6e4ba09
Chaw Thiri San <chawthiri177@gmail.com>
2023-03-31 22:46:41 +0900

    array 4 code explanation and testcases

--- array/smallest subarray/solution.py
+++ array/smallest subarray/solution.py
@@ -1,16 +1,18 @@
-# time O(n), space = O(1)
+# time O(n), loop through the array only once.
+# space = O(1) : constant space 
 def subarraySort(array):
     minOutOfOrder = float("inf")
     maxOutOfOrder = float("-inf")
     for i in range(len(array)):
         num = array[i]
         if isOutOfOrder(i,num,array):
-            minOutOfOrder = min(minOutOfOrder,num)
+            minOutOfOrder = min(minOutOfOrder,num) # the first num i see 
             maxOutOfOrder = max(maxOutOfOrder,num)
     
     if minOutOfOrder == float("inf"):
         return [-1,-1]
     
+    # find the idx from the min and max values
     subarrayLeftIdx = 0
     while minOutOfOrder >= array[subarrayLeftIdx]:
         subarrayLeftIdx += 1
@@ -22,6 +24,8 @@ def subarraySort(array):
     return[subarrayLeftIdx,subarrayRightIdx]
 
 def isOutOfOrder(i, num, array):
+    # if the index is 0, check only right side, if index is -1, check only left 
+    # else check both sides.
     if i == 0:
         return num> array[i+1]
     if i == len(array)-1:

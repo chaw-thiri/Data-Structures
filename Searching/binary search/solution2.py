a6f0f652737b82ddfb786b01c1d01c6aff608adf
Chaw Thiri San <chawthiri177@gmail.com>
2023-04-10 16:28:29 +0900

    binary search using iteration update

---
+++ Searching/binary search/solution2.py
@@ -0,0 +1,11 @@
+def binarySearch(array, target):
+    left, right = 0 , len(array)-1
+    while left <= right:
+        mid = (left+right) // 2
+        if target < array[mid]:
+            right = mid - 1
+        elif target > array[mid]:
+            left = mid + 1
+        else:
+            return mid 
+    return -1

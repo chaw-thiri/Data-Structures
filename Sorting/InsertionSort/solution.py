beff930ca6ad230576e42d27d7c02a46f753bc56
Chaw Thiri San <chawthiri177@gmail.com>
2023-04-10 17:37:14 +0900

    add comments

--- Sorting/InsertionSort/solution.py
+++ Sorting/InsertionSort/solution.py
@@ -1,9 +1,13 @@
+# time = O(n) best , O(n ^ 2) worst : when the smallest number is at the end.
+
 def insertionSort(array):
     for i in range(1,len(array)):   
         h = i
-        while h > 0 and array[h-1]> array[h]: # when h < 0 the array would be all sorted
+        while h > 0 and array[h-1]> array[h]: 
+            # when h < 0 the array would be all sorted after going all backwards
             array[h],array[h-1] = array[h-1],array[h]
-            h -= 1
+            h -= 1 # to recheck the front part of the array after current alteration.
+
     return array
 array = [3,7,5,8,4,2,6,3,9]
 print(insertionSort(array))

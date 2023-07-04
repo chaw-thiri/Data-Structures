82bc041f129242c5fb0155fff49fa7caf3bd18e2
Chaw Thiri San <chawthiri177@gmail.com>
2023-07-04 17:29:09 +0900

    add heap sort

---
+++ Sorting/heap sort/solution.py
@@ -0,0 +1,9 @@
+# time - O(n log n) , S - O(1)
+def heapSort(array):
+    pass
+def buildMaxHeap(array):
+    pass
+def siftDown(curIdx, endIdx, heap):
+    childOneIdx = curIdx * 2 + 1
+    while childOneIdx <= endIdx:
+        

68ee497214759301abe06888edd4aac68beab9ab
Chaw Thiri San <chawthiri177@gmail.com>
2023-07-05 16:23:12 +0900

    add cmt to .py of minWaitingTime

--- greedy algorithms/solution.py
+++ greedy algorithms/solution.py
@@ -1,9 +1,15 @@
+# T = O(n log n), due to sorting 
+# S = O(1)
 def minimumWaitingTime(queries):
-    queries.sort()
+    queries.sort() # t = n log n 
 
     totalWaitingTime = 0
-    for idx, duration in enumerate(queries):
-        queriesleft = len(queries)-(idx+1)
+
+    for idx, duration in enumerate(queries): # n
+        queriesleft = len(queries)-(idx+1)   # idx = +1 cuz idx starts at 0
         totalWaitingTime += duration * queriesleft
 
     return totalWaitingTime
+
+
+# since n log n > n , net = O(n log n)

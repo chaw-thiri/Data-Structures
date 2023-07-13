8bd0d5d943d53f459ef34699c52e8d43ab9f6063
Chaw Thiri San <chawthiri177@gmail.com>
2023-07-13 20:32:50 +0900

    add my sols(not optimal space and time)

---
+++ binary tree/height balanced tree/solution.py
@@ -0,0 +1,33 @@
+# time = O(n)
+# space = O(n) , n = number of nodes in the tree
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def heightBalancedBinaryTree(tree):
+    if tree is None:
+        return True
+    return heightBalancedBinaryTreeHelper(tree)
+
+def heightBalancedBinaryTreeHelper(tree):
+    if tree is None:
+        return True
+
+    leftHeight = getHeight(tree.left)
+    rightHeight = getHeight(tree.right)
+
+    if (
+        abs(leftHeight - rightHeight) > 1 or
+        not heightBalancedBinaryTreeHelper(tree.left) or
+        not heightBalancedBinaryTreeHelper(tree.right)
+    ):
+        return False
+
+    return True
+
+def getHeight(tree):
+    if tree is None:
+        return 0
+    return max(getHeight(tree.left), getHeight(tree.right)) + 1
---
+++ binary tree/height balanced tree/solution3.py
@@ -0,0 +1,28 @@
+# not complete yet
+# 12/ 14 test case 
+# test 2 and test 10 fail
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def heightBalancedBinaryTree(tree):
+    if tree is None:
+        return False 
+    return heightBalancedBinaryTreeHelper(tree)
+        
+def heightBalancedBinaryTreeHelper(tree):
+    if tree.left is not None and tree.right is not None:
+        return heightBalancedBinaryTreeHelper(tree.left)
+        return heightBalancedBinaryTreeHelper(tree.right)
+    # tree.left has one extra node now
+    # if there is just one more extra node, the tree would not balanced
+    elif tree.left is not None and tree.right is None:
+        return (tree.left.left is None and tree.left.right is None)
+    elif tree.right is not None and tree.left is None:
+        return (tree.right.left is None and tree.right.right is None)
+    return True
+
